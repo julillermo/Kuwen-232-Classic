e4477ab5a95b3eaf3fa34266f6a796b21735a9de
julillermo <julius.guillermo.work@gmail.com>
2024-08-02 00:32:31 +0800

    fix font loading

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,4 +1,3 @@
-import { Global, css } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,7 +7,6 @@ const { versions } = window;
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
@@ -34,39 +32,6 @@ console.log(
 console.log("node version:", versions.node());
 console.log("chrome version:", versions.chrome());
 console.log("node version:", versions.node());
-// console.log("IPC ping:", await versions.ping());
-
-console.log("window innerWidth", window.innerWidth);
-console.log("window innerHeight", window.innerHeight);
-
-function GlobalStyle() {
-  return (
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Comic Neue";
-          src: url("./assets/fonts/Comic_Neue/ComicNeue-Regular.ttf");
-        }
-
-        html,
-        body {
-          margin: 0 0 0 0;
-          padding: 0 0 0 0;
-
-          background-color: #282c34;
-          color: #abb2bf;
-
-          font-family: "Comic Neue";
-        }
-
-        input,
-        textarea {
-          font-family: "Comic Neue";
-        }
-      `}
-    />
-  );
-}
 
 // Adding typing to the added objects in the global names
 declare global {
